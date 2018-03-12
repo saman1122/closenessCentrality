3bb9a3984f344d9f61d9cdee498c0746dca01da0
saman1122 <saman11@hotmail.fr>
2018-03-12 11:23:37 +0100

    liens uniques

--- src/main/java/fr/insta/saman/App.java
+++ src/main/java/fr/insta/saman/App.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -23,7 +24,7 @@ public class App
             result= stream
                     .filter(line -> {
                         Integer secondes = Integer.parseInt((line.split(" "))[2]);
-                        return (secondes >= 1200) && (secondes <= 1800);
+                        return (secondes >= 1200) && (secondes <= 2400);
                     })
                     .collect(Collectors.toList());
 
@@ -32,6 +33,21 @@ public class App
             e.printStackTrace();
         }
 
-        result.forEach(System.out::println);
+        //result.forEach(System.out::println);
+
+        List<String> reformat = new ArrayList<>();
+
+        result.forEach(x-> {
+            String[] tab = x.split(" ");
+            if (Integer.parseInt(tab[1]) < Integer.parseInt(tab[0])) {
+                reformat.add(tab[1] + " " + tab[0]);
+            } else {
+                reformat.add(tab[0] + " " + tab[1]);
+            }
+        });
+
+        List<String> liensUniques = reformat.stream().sorted().distinct().collect(Collectors.toList());
+
+        //liensUniques.forEach(System.out::println);
     }
 }
